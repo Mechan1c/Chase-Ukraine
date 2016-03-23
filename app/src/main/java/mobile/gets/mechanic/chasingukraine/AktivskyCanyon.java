a384b3eb24714410e083115ce9bbd65bc20d8aae
Mechan1c <harden@ua.fm>
2016-03-23 21:56:48 +0200

    fixed OnClickListener logic

--- app/src/main/java/mobile/gets/mechanic/chasingukraine/AktivskyCanyon.java
+++ app/src/main/java/mobile/gets/mechanic/chasingukraine/AktivskyCanyon.java
@@ -2,13 +2,12 @@ package mobile.gets.mechanic.chasingukraine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class AktivskyCanyon extends AppCompatActivity {
+public class AktivskyCanyon extends AppCompatActivity implements View.OnClickListener {
 
     TextView tvDesc, tvType, tvTypeVal, tvLoc, tvLocVal;
     Button bSub;
@@ -18,44 +17,31 @@ public class AktivskyCanyon extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aktivsky_canyon);
 
-        View.OnClickListener listenToToast = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                this.showToast(v);
-            }
-
-            public void showToast(View v) {
-                String msg = String.valueOf(tvLocVal.getText()) ;
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        msg ,
-                        Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.CENTER, 0, 0);
-                toast.show();
-            }
-        };
-
         tvDesc = (TextView) findViewById(R.id.tvDescription);
         bSub = (Button) findViewById(R.id.bSubmit);
-        tvType =(TextView) findViewById(R.id.type);
-        tvTypeVal =(TextView) findViewById(R.id.typeVal);
-        tvLoc =(TextView) findViewById(R.id.location);
-        tvLocVal =(TextView) findViewById(R.id.locationVal);
-
-        bSub.setOnClickListener(listenToToast);
-        tvDesc.setOnClickListener(listenToToast);
-        tvType.setOnClickListener(listenToToast);
-        tvTypeVal.setOnClickListener(listenToToast);
-        tvLoc.setOnClickListener(listenToToast);
-        tvLocVal.setOnClickListener(listenToToast);
-
-
+        tvType = (TextView) findViewById(R.id.type);
+        tvTypeVal = (TextView) findViewById(R.id.typeVal);
+        tvLoc = (TextView) findViewById(R.id.location);
+        tvLocVal = (TextView) findViewById(R.id.locationVal);
+        bSub.setOnClickListener(this);
+        tvDesc.setOnClickListener(this);
+        tvType.setOnClickListener(this);
+        tvTypeVal.setOnClickListener(this);
+        tvLoc.setOnClickListener(this);
+        tvLocVal.setOnClickListener(this);
     }
 
     /*Closing app on back button*/
 
     @Override
-    public void onBackPressed()
-    {
+    public void onBackPressed() {
         finish();
+    }
+
+        @Override
+    public void onClick(View v) {
+        Toast.makeText(getApplicationContext(),
+                v.getClass().getSimpleName(),
+                Toast.LENGTH_SHORT).show();
     }
 }
